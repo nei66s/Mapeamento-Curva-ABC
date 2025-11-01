b7f7405a10113624c94be948e26c94a1ba745603
Valdinei Lima <bsilva66h@gmail.com>
2025-11-01 17:30:43 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/dashboard/incidents/incident-map.tsx
+++ src/components/dashboard/incidents/incident-map.tsx
@@ -1,26 +1,10 @@
 'use client';
 
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
-import iconUrl from 'leaflet/dist/images/marker-icon.png';
-import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 import { useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import type { Incident } from '@/lib/types';
-
-// Solução para o ícone padrão do Leaflet não aparecer corretamente no Next.js
-const DefaultIcon = L.icon({
-    iconRetinaUrl: iconRetinaUrl.src,
-    iconUrl: iconUrl.src,
-    shadowUrl: shadowUrl.src,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface IncidentMapProps {
   incidents: Incident[];
@@ -29,13 +13,10 @@ interface IncidentMapProps {
 export default function IncidentMap({ incidents }: IncidentMapProps) {
   const Map = useMemo(
     () =>
-      dynamic(
-        () => import('@/components/dashboard/incidents/map'),
-        {
-          loading: () => <p>A map is loading</p>,
-          ssr: false,
-        }
-      ),
+      dynamic(() => import('@/components/dashboard/incidents/map'), {
+        loading: () => <Skeleton className="h-full w-full" />,
+        ssr: false,
+      }),
     []
   );
 
