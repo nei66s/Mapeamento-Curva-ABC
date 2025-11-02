2f3c0553fde993a16b99bbf279793a3e636430b6
Valdinei Lima <bsilva66h@gmail.com>
2025-11-02 14:58:30 +0000

    onde eu coloco o logo na pasta para que fique ali?

--- next.config.ts
+++ next.config.ts
@@ -29,12 +29,7 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
+      
     ],
   },
   experimental: {
